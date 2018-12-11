98d4aeec846e811bbdd3dc6ae855907e298a3de5
darkdubdeep <darkdubdeep@gmail.com>
2018-12-11 23:58:05 +0400

    launch component addet

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import "./App.css";
 import logo from "./space-x-logo.jpg";
 import ApolloClient from "apollo-boost";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import Launches from "./components/Launches";
+import Launch from "./components/Launch";
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql"
 });
@@ -12,14 +14,17 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="container">
-          <img
-            src={logo}
-            alt="spacex"
-            style={{ width: 300, display: "block", margin: "auto" }}
-          />
-          <Launches />
-        </div>
+        <Router>
+          <div className="container">
+            <img
+              src={logo}
+              alt="spacex"
+              style={{ width: 300, display: "block", margin: "auto" }}
+            />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
